00c750d6a32f1eae3a3c80434956632ed426912d
Qianhongbo <qianhomeberg@gmail.com>
2022-07-18 14:52:14 -0700

    dp

--- problems/322.coin-change.cpp
+++ problems/322.coin-change.cpp
@@ -5,28 +5,66 @@
  */
 
 #include <vector>
+#include <limits.h>
 using namespace std;
 
 // @lc code=start
 class Solution {
  public:
+  std::vector<int> memo;
+  // top down
   int coinChange(vector<int>& coins, int amount) {
-    // the reason why we use amout + 1 to initialize dp
-    // is that the maximum value for us to change the coin is amount
-    // which is all use one dollor coin
-    // amount + 1 represents infinity
-    // however we can't use INT_MAX because we also need to + 1 
-    // this will cause integer overflow
-    std::vector<int> dp(amount + 1, amount + 1);
-    dp[0] = 0;
-    for (int i{1}; i <= amount; i++) {
-      for (const auto& coin : coins) {
-        if (coin <= i) {
-          dp[i] = std::min(dp[i], dp[i - coin] + 1);
-        }
-      }
-    }
-    return dp[amount] == amount + 1 ? -1 : dp[amount];
+    memo.resize(amount + 1, -666);
+
+    return dp(coins, amount);
   }
+
+  int dp(vector<int>& coins, int amount) {
+    if (0 == amount) {
+      return 0;
+    }
+
+    if (amount < 0) {
+      return -1;
+    }
+
+    if (memo[amount] != -666) {
+      return memo[amount];
+    }
+
+    int res = INT_MAX;
+    for (const auto& coin: coins) {
+      int subProblem = dp(coins, amount - coin);
+      if (-1 == subProblem) {
+        continue;
+      }
+
+      res = min(res, subProblem + 1);
+    }
+
+    memo[amount] = res == INT_MAX ? -1 : res;
+
+    return memo[amount];
+  }
+
+  // bottom up
+  // int coinChange(vector<int>& coins, int amount) {
+  //   // the reason why we use amout + 1 to initialize dp
+  //   // is that the maximum value for us to change the coin is amount
+  //   // which is all use one dollor coin
+  //   // amount + 1 represents infinity
+  //   // however we can't use INT_MAX because we also need to + 1 
+  //   // this will cause integer overflow
+  //   std::vector<int> dp(amount + 1, amount + 1);
+  //   dp[0] = 0;
+  //   for (int i{1}; i <= amount; i++) {
+  //     for (const auto& coin : coins) {
+  //       if (coin <= i) {
+  //         dp[i] = std::min(dp[i], dp[i - coin] + 1);
+  //       }
+  //     }
+  //   }
+  //   return dp[amount] == amount + 1 ? -1 : dp[amount];
+  // }
 };
 // @lc code=end
--- problems/509.fibonacci-number.cpp
+++ problems/509.fibonacci-number.cpp
@@ -4,25 +4,54 @@
  * [509] Fibonacci Number
  */
 
-using namspace std;
+#include <vector>
+using namespace std;
 
 // @lc code=start
 class Solution {
  public:
+  // top down
+  // int fib(int n) {
+  //   if (n == 0 || n == 1) {
+  //     return n;
+  //   }
+  //   return fib(n - 1) + fib(n - 2);
+  // }
+  
+  // using memo
   int fib(int n) {
-    if ( n == 0 || n == 1) {
+    std::vector<int> memo(n + 1, -1);
+    return helper(memo, n);
+  }
+
+  int helper(std::vector<int>& memo, int n) {
+    if (n == 0 || n == 1) {
       return n;
     }
 
-    int dp_1 = 0;
-    int dp_2 = 1;
-    for (int i{1}; i < n; i++) {
-      int temp = dp_1 + dp_2;
-      dp_1 = dp_2;
-      dp_2 = temp;
+    if (-1 != memo[n]) {
+      return memo[n];
     }
 
-    return dp_2;
+    memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+    return memo[n];
   }
+
+  // bottom up
+  // int fib(int n) {
+  //   if (n == 0 || n == 1) {
+  //     return n;
+  //   }
+
+  //   int dp_1 = 0;
+  //   int dp_2 = 1;
+  //   for (int i{1}; i < n; i++) {
+  //     int temp = dp_1 + dp_2;
+  //     dp_1 = dp_2;
+  //     dp_2 = temp;
+  //   }
+
+  //   return dp_2;
+  // }
 };
 // @lc code=end
